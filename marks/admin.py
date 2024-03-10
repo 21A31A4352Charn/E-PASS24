898bbf8bd3b8f8e2766a9a5d8f15e0ceca080d08
21A31A4352Charn <141161368+21A31A4352Charn@users.noreply.github.com>
2024-03-10 12:11:49 +0530

    Delete marks directory

--- marks/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import student,mark
-# Register your models here.
-admin.site.register(student)
-admin.site.register(mark)
--- marks/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class SubscribeForm(forms.Form):
-    email = forms.EmailField()
-
-    def __str__ (self):
-        return self.email
